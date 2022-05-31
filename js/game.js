18361ac4c0c377684bbe2afdf3c258a70870a15a
samuelcarlson18 <99274631+samuelcarlson18@users.noreply.github.com>
2022-05-31 15:07:47 +0000

    Added comments

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
 /* global Phaser */
 
+//The size of the screen and the background color
 const config = {
   type: Phaser.AUTO,
   width: 1920,
@@ -7,5 +8,6 @@ const config = {
   backgroundColor: 0x5f6e7a
 }
 
+//Showing that my game is using the phaser library
 const game = new Phaser.Game(config)
 console.log(game)
