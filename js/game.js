19e570f043b4da87cdcebd12b35d6c40459a067f
samuelcarlson18 <99274631+samuelcarlson18@users.noreply.github.com>
2022-06-01 14:32:54 +0000

    added comments to files

--- js/game.js
+++ js/game.js
@@ -1,13 +1,30 @@
 /* global Phaser */
 
-//The size of the screen and the background color
+//Copyright (c) 2022 Samuel carlson All rights reserved
+//
+//Created by: Samuel Carlson
+//Created on: June 2022
+//This is thePhaser3 configuration file
+
+//* Game scene */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  // set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
-//Showing that my game is using the phaser library
 const game = new Phaser.Game(config)
 console.log(game)
