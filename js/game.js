084a2a2c4e5230e983c7e4dfb7ed6e784054b8bb
samuelcarlson18 <99274631+samuelcarlson18@users.noreply.github.com>
2022-06-02 14:48:05 +0000

    updated favicons

--- js/game.js
+++ js/game.js
@@ -6,26 +6,28 @@
 //Created on: June 2022
 //This is thePhaser3 configuration file
 
-import SplashScene from './splashScene.js'
-import TitleScene from './titleScene.js'
+//import SplashScene from './splashScene.js'
+//import TitleScene from './titleScene.js'
 
 // Our game scenes
-const splashScene = new SplashScene()
-const titleScene = new TitleScene()
+//const splashScene = new SplashScene()
+//const titleScene = new TitleScene()
 
-//* Game scene */
+//* Configuration settings for game scene */
 const config = {
   type: Phaser.AUTO,
+  // The width and the height of the screen in pixels
   width: 1920,
   height: 1080,
   physics: {
     default: 'arcade',
     arcade: {
+      // set to true to see the physics
       debug: true
     }
   },
-  // set background color
-  backgroundColor: 0x5f6e7a,
+  // set background color to grey
+  backgroundColor: 0xD73178,
   scale: {
     mode: Phaser.Scale.FIT,
     // we place it in the middle of the page.
@@ -34,11 +36,12 @@ const config = {
 }
  
 const game = new Phaser.Game(config)
+console.log(game)
 
 // load scenes
 // NOTE: remember any "key" is global and CAN NOT be reused!
-game.scene.add('splashScene', splashScene)
-game.scene.add('titleScene', titleScene)
+//game.scene.add('splashScene', splashScene)
+//game.scene.add('titleScene', titleScene)
 
 // start title
-game.scene.start('splashScene')
+//game.scene.start('splashScene')
