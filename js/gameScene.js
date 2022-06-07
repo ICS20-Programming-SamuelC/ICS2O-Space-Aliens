e841ed2b4617edacbae3bc1ef7cd2dd380affee5
samuelcarlson18 <99274631+samuelcarlson18@users.noreply.github.com>
2022-06-07 14:47:22 +0000

    updated game scene

--- js/gameScene.js
+++ js/gameScene.js
@@ -6,23 +6,56 @@
 // Created on: June 2022
 // This is the Menu Scene
 
+//assigning the game scene background variable to null and assigning the ship image variable to null
 class GameScene extends Phaser.Scene {
   constructor () {
     super({ key: 'gameScene' })
+
+    this.background = null
+    this.ship = null
   }
 
   init (data) {
     this.cameras.main.setBackgroundColor('#D74731')
   }
 
+  //loading the artwork for the game scene
   preload () {
     console.log('Game Scene')
+
+    //images
+    this.load.image('starBackground', 'assets/starBackground.png')
+    this.load.image('ship', 'assets/spaceShip.png')
+    
   }
 
+  //show the game scene images to the user and setting where the images should be on the screen
   create (data) {
+    this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
+    this.background.setOrigin(0, 0)
+
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
   }
 
+  //checking user input 60 times per second and exacuting what they have clicked. If they have moved left or right
   update (time, delta) {
+
+    const keyLeftObj = this.input.keyboard.addKey('LEFT')
+    const keyRightObj = this.input.keyboard.addKey('RIGHT')
+
+    if (keyLeftObj.isDown === true) {
+      this.ship.x -= 15
+      if (this.ship.x < 0) {
+        this.ship.x = 0
+      }
+    }
+
+    if (keyRightObj.isDown === true) {
+      this.ship.x += 15
+      if (this.ship.x > 1920) {
+        this.ship.x = 1920
+      }
+    }
   }
 }
 
