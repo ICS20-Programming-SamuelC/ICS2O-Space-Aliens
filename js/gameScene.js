af8768839476f71d09dbe6a3a27b034f4e5bb706
samuelcarlson18 <99274631+samuelcarlson18@users.noreply.github.com>
2022-06-10 15:01:18 +0000

    updated game scene file

--- js/gameScene.js
+++ js/gameScene.js
@@ -29,6 +29,9 @@ class GameScene extends Phaser.Scene {
     this.score = 0
     this.scoreText = null
     this.scoreTextStyle = { font: '65px Arial', fill: '#ffffff', align: 'center'}
+
+    this.gameOverText = null
+    this.gameOverTextStyle = { font: '65px Arial', fill: '#ff0000', align: 'center'}
   }
 
   init (data) {
@@ -47,6 +50,7 @@ class GameScene extends Phaser.Scene {
     //setting the sound for the missiles and explosion
     this.load.audio('laser', 'assets/laser1.wav')
     this.load.audio('explosion', 'assets/barrelExploding.wav')
+    this.load.audio('bomb', 'assets/bomb.wav')
   }
 
   //show the game scene images to the user and setting where the images should be on the screen
@@ -74,6 +78,17 @@ class GameScene extends Phaser.Scene {
       this.scoreText.setText('Score: ' + this.score.toString())
       this.createAlien()
       this.createAlien()
+    }.bind(this))
+
+      //code to lose the game when an alien collides with the ship
+      this.physics.add.collider(this.ship, this.alienGroup, function (shipCollide, alienCollide) {
+      this.sound.play('bomb')
+      this.physics.pause()
+      alienCollide.destroy()
+      shipCollide.destroy()
+      this.gameOverText = this.add.text(1920 / 2, 1080 / 2, 'Game Over!\nClick to play again.', this.gameOverTextStyle).setOrigin(0.5)
+      this.gameOverText.setInteractive({ useHandCursor: true})
+      this.gameOverText.on('pointerdown', () => this.scene.start('gameScene'))
     }.bind(this))
   }
 
